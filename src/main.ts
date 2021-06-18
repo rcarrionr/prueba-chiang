b50d444a6a0b82b467da5e70903d34debed9537d
Richard Carrion <richard.carrion.r@gmail.com>
2021-06-18 02:31:08 -0500

    docker config

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import * as mongoose from 'mongoose';
 mongoose.set('debug', true);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
